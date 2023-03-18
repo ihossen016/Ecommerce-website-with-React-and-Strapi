88d073728f35a50a2a74207230783543c5289108
ihossen016 <ihossen016@gmail.com>
2023-03-19 00:00:56 +0600

    homepage slider added

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MainCarousel from "./MainCarousel";
 
 const Home = () => {
-    return <div></div>;
+    return (
+        <div className="home">
+            <MainCarousel />
+        </div>
+    );
 };
 
 export default Home;
