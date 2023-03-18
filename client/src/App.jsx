486c4c73a3dabe7eac5fce79933ad3d2b399b340
ihossen016 <ihossen016@gmail.com>
2023-03-18 18:22:02 +0600

    folder structre organized

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,33 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./scenes/global/Navbar";
+import Home from "./scenes/home/Home";
+
+const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+};
+
 function App() {
-    return <div className="app"></div>;
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <Navbar />
+                <ScrollToTop />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="item/:itemId" element={<ItemDetails />} />
+                    <Route path="checkout" element={<Checkout />} />
+                    <Route path="checkout/success" element={<Confirmation />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
