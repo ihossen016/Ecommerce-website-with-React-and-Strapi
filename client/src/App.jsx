70b0dac1ba59023122f33a33a8f8dd9713be0b36
ihossen016 <ihossen016@gmail.com>
2023-03-18 22:42:17 +0600

    off canvas cart menu added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
+import CartMenu from "./scenes/global/CartMenu";
 import Navbar from "./scenes/global/Navbar";
 import Home from "./scenes/home/Home";
 import ItemDetails from "./scenes/itemDetails/itemDetails";
@@ -28,6 +29,7 @@ function App() {
                     <Route path="checkout" element={<Checkout />} />
                     <Route path="checkout/success" element={<Confirmation />} />
                 </Routes>
+                <CartMenu />
             </BrowserRouter>
         </div>
     );
