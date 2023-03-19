5a59303223b232c1af05310b15fecf9c60004a01
ihossen016 <ihossen016@gmail.com>
2023-03-19 22:46:57 +0600

    footer component added

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
                     sx={{ "&:hover": { cursor: "pointer" } }}
                     color={shades.secondary[500]}
                 >
-                    Shoppy
+                    SHOPPY
                 </Box>
                 <Box
                     display="flex"
