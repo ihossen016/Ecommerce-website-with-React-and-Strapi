bc4386b29561709071be443ffd2dfc5d88915fd4
ihossen016 <ihossen016@gmail.com>
2023-03-19 22:37:51 +0600

    shopping list component added

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MainCarousel from "./MainCarousel";
+import ShoppingList from "./ShoppingList";
 
 const Home = () => {
     return (
         <div className="home">
             <MainCarousel />
+            <ShoppingList />
         </div>
     );
 };
