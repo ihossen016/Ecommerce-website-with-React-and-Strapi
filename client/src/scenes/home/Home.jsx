379a87412452071d428f19aa31c904bedfa0b301
ihossen016 <ihossen016@gmail.com>
2023-03-19 22:39:48 +0600

    subscribe component added

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MainCarousel from "./MainCarousel";
 import ShoppingList from "./ShoppingList";
+import Subscribe from "./Subscribe";
 
 const Home = () => {
     return (
         <div className="home">
             <MainCarousel />
             <ShoppingList />
+            <Subscribe />
         </div>
     );
 };
